f70d496a6922e742684ef5bbb7d22c99cec7c940
Hannah <2hbrown13@gmail.com>
2020-11-17 09:02:39 -0600

    testing

--- BrownJDBCBank/src/test/java/com/revature/test/MenuTest.java
+++ BrownJDBCBank/src/test/java/com/revature/test/MenuTest.java
@@ -6,14 +6,14 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
-//import org.junit.Test;
+import org.junit.Test;
 
 import com.revature.beans.Accounts;
 import com.revature.beans.Customer;
 import com.revature.dao.CustomerDAO;
 import com.revature.daoimpl.CustomerDAOImpl;
 
-public class Test {
+public class MenuTest {
 	@BeforeClass
 	public static void intitialSetUp() {
 		System.out.println("I run once before everything");
@@ -22,32 +22,34 @@ public class Test {
 	public void runBeforeEachTest() {
 		System.out.println("I run before each test");
 	}
-	//@Test
+	@Test
 	public void test() {
 		assert(true);
 	}
-	//@Test
+	@Test
 	public void test2() {
 		assertEquals(3,3);
 	}
 
-	//@Test
+	@Test
 	public void updateLastName() throws SQLException {
 		CustomerDAO cdi = new CustomerDAOImpl();
 		Customer c = cdi.getCustomerByName("Hannah");
 		c.setLastName("Stegemeier");
+		@SuppressWarnings("static-access")
 		String result = c.getLastName();
 		assertEquals(result,"Stegemeier");
 		
 	}
-	//@Test
-	//public void deleteRow() throws SQLException {
-	//	CustomerDAO cdi = new CustomerDAOImpl();
-	//	Customer c = cdi.deleteCustomer("hbrown");
-	//	String result = c.toString();
-	//	assertEquals(result, null);
-	//}
-	//@Test
+	@Test
+	public void deleteRow() throws SQLException {
+		//CustomerDAO cdi = new CustomerDAOImpl();
+		//Customer c = cdi.deleteCustomer(null);
+		//String result = c.toString();
+		//assertEquals(result, null);
+	}
+	@SuppressWarnings("deprecation")
+	@Test
 	public void testDeposit() {
 		Accounts a = new Accounts(20,5000);
 		double deposit = 200;
@@ -61,3 +63,4 @@ public class Test {
 	}
 	
 }
+
